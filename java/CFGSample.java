f1485af9e1977cecd2d179ec6db07b8c518815d0
alancleary <acleary@ncgr.org>
2023-12-14 11:34:59 -0700

    Added timing code to CFGSample

--- java/CFGSample.java
+++ java/CFGSample.java
@@ -9,9 +9,19 @@ public class CFGSample {
             String filename = args[0];
             cfg = CFG.fromMrRepairFile(filename);
 
-            for (int i = 0; i < cfg.textLength; i++) {
-                System.out.print(cfg.get(i));
+            long startTime, endTime, duration = 0;
+            char c;
+            for (int i = 0; i < cfg.textLength(); i++) {
+                startTime = System.nanoTime();
+                c = cfg.get(i);
+                endTime = System.nanoTime();
+
+                System.out.print(c);
+
+                duration += (endTime - startTime);  //divide by 1000000 to get milliseconds.
+
             }
+            System.err.println("average query time: " + (duration / cfg.textLength()) + " ns");
         } catch (Exception e) {
             System.out.println(e);
             e.printStackTrace();

ad72ad6f237dee6217684c5f5882b984796e1c93
alancleary <acleary@ncgr.org>
2024-06-12 14:42:52 -0600

    Added an abstract class for V2 of the random access algorithm

---
+++ cpp/include/cfg/random_access_v2.hpp
@@ -0,0 +1,38 @@
+#ifndef INCLUDED_CFG_RANDOM_ACCESS_V2
+#define INCLUDED_CFG_RANDOM_ACCESS_V2
+
+#include <ostream>
+#include "cfg/cfg.hpp"
+
+namespace cfg {
+
+/** An abstract class that adds random access support to a CFG. */
+class RandomAccessV2
+{
+    private:
+
+        virtual void rankSelect(uint64_t i, int& rank, uint64_t& select) = 0;
+        virtual uint64_t expansionSize(int rule) = 0;
+
+    protected:
+
+        CFG* cfg;
+
+    public:
+
+        RandomAccessV2(CFG* cfg): cfg(cfg) { };
+
+        /**
+          * Gets a substring in the original string.
+          *
+          * @param out The output stream to write the substring to.
+          * @param begin The start position of the substring in the original string.
+          * @param end The end position of the substring in the original string.
+          * @throws Exception if begin or end is out of bounds.
+          */
+        void get(std::ostream& out, uint64_t begin, uint64_t end);
+};
+
+}
+
+#endif

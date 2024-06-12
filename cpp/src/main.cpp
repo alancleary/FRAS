1dcc566c3ad073f7867647b2523a321097086af9
alancleary <acleary@ncgr.org>
2024-06-12 15:04:11 -0600

    Updated main to test V2 random access algorithm

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -3,11 +3,10 @@
 #include <random>
 
 #include "cfg/cfg.hpp"
-#include "cfg/random_access_amt.hpp"
-#include "cfg/random_access_bv.hpp"
-#include <sdsl/bit_vectors.hpp>
-#include <sdsl/rank_support_v.hpp>
-#include <sdsl/select_support_mcl.hpp>
+//#include "cfg/random_access_amt.hpp"
+//#include "cfg/random_access_bv.hpp"
+//#include "cfg/random_access_v2_bv.hpp"
+#include "cfg/random_access_v2_sd.hpp"
 
 using namespace std;
 using namespace cfg;
@@ -57,18 +56,28 @@ int main(int argc, char* argv[])
     cerr << "rules size: " << cfg->getRulesSize() << endl;
     cerr << "total size: " << cfg->getTotalSize() << endl;
     cerr << "depth: " << cfg->getDepth() << endl;
+    uint64_t cfgMemSize = cfg->memSize();
+    cerr << "mem size: " << cfgMemSize << endl;
 
-    // instantiate grammars
-    RandomAccessAMT amt(cfg);
-    RandomAccessBV<sdsl::bit_vector, sdsl::rank_support_v5<>, sdsl::select_support_mcl<>> bv(cfg);
+    // instantiate indexes
+    //RandomAccessAMT amt(cfg);
+    //RandomAccessBV<sdsl::bit_vector, sdsl::rank_support_v5<>, sdsl::select_support_mcl<>> bv(cfg);
+    //RandomAccessV2BV<sdsl::bit_vector, sdsl::rank_support_v5<>, sdsl::select_support_mcl<>> bv2(cfg);
+    RandomAccessV2SD sd(cfg);
+    uint64_t sdMemSize = sd.memSize();
+    cerr << "sdv2 mem size: " << sdMemSize << endl;
+
+    cerr << "total mem size: " << cfgMemSize + sdMemSize << endl;
     
     // generate the original text
     //cfg->get(cout, 0, cfg->getTextLength() - 1);
 
     // benchmarks
     chrono::steady_clock::time_point startTime, endTime;
-    uint64_t durationAMT = 0;
-    uint64_t durationBV = 0;
+    //uint64_t durationAMT = 0;
+    //uint64_t durationBV = 0;
+    //uint64_t durationBV2 = 0;
+    uint64_t durationSD = 0;
     uint64_t numQueries = 10000, querySize = 1000;
     random_device rd;
     mt19937 gen(rd());
@@ -81,24 +90,43 @@ int main(int argc, char* argv[])
         end = begin + querySize - 1;
 
         // AMT
-        startTime = chrono::steady_clock::now();
-        amt.get(cout, begin, end);
-        endTime = chrono::steady_clock::now();
-        durationAMT += chrono::duration_cast<chrono::microseconds>(endTime - startTime).count();
+        //startTime = chrono::steady_clock::now();
+        //amt.get(cout, begin, end);
+        //endTime = chrono::steady_clock::now();
+        //durationAMT += chrono::duration_cast<chrono::microseconds>(endTime - startTime).count();
 
         // BV
+        //startTime = chrono::steady_clock::now();
+        //bv.get(cout, begin, end);
+        //endTime = chrono::steady_clock::now();
+        //durationBV += chrono::duration_cast<chrono::microseconds>(endTime - startTime).count();
+
+        //begin = distr(gen);
+        //end = begin + querySize - 1;
+
+        // BV2
+        //startTime = chrono::steady_clock::now();
+        //bv2.get(cout, begin, end);
+        //endTime = chrono::steady_clock::now();
+        //durationBV2 += chrono::duration_cast<chrono::microseconds>(endTime - startTime).count();
+
+        // SD
         startTime = chrono::steady_clock::now();
-        bv.get(cout, begin, end);
+        sd.get(cout, begin, end);
         endTime = chrono::steady_clock::now();
-        durationBV += chrono::duration_cast<chrono::microseconds>(endTime - startTime).count();
+        durationSD += chrono::duration_cast<chrono::microseconds>(endTime - startTime).count();
 
         begin = distr(gen);
         end = begin + querySize - 1;
     }
 
-    cerr << "average AMT query time: " << durationAMT / numQueries << "[µs]" << endl;
+    //cerr << "average AMT query time: " << durationAMT / numQueries << "[µs]" << endl;
 
-    cerr << "average BV query time: " << durationBV / numQueries << "[µs]" << endl;
+    //cerr << "average BV query time: " << durationBV / numQueries << "[µs]" << endl;
+
+    //cerr << "average BV2 query time: " << durationBV2 / numQueries << "[µs]" << endl;
+
+    cerr << "average SD query time: " << durationSD / numQueries << "[µs]" << endl;
 
     delete cfg;
 
